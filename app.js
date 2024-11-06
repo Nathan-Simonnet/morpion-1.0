80150cb8f720c417f878cf711dbeae24f2f5750d
Nathan-Simonnet <nathan.simonnet@gmail.com>
2024-11-06 18:45:57 +0100

    Html/css done. JS selectors done. todo: well, game logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const currentGame = ["","","","","","","","",""];
 
+
+const currentGame = ["", "", "", "", "", "", "", "", ""];
 
 const winningCombinations = [
     [0, 1, 2],
@@ -12,4 +13,30 @@ const winningCombinations = [
     [2, 4, 6]
 ]
 
+
+let currentPlayer = "X";
+
+function switchPlayer() {
+    currentPlayer === "X" ? currentPlayer = "O" : currentPlayer = "X"
+}
+
+const grid = document.querySelector('.grid');
+const square = document.querySelector('.square');
+const marker = document.querySelector('.marker');
+
+console.log(grid, square, marker)
+
+
+function gridHandler(e) {
+    if(e.target.classList.contains("marker")){
+        const squarreElement = e.target;
+        const squarreNumber = squarreElement.dataset.number;
+        squarreElement.classList.add('clicked')
+        console.log(squarreElement, squarreNumber);
+        squarreElement.textContent = currentPlayer;
+        switchPlayer()
+    }
+}
+
+grid.addEventListener('click', gridHandler);
 
