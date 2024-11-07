447664de53f1c81e91a423d69de129365eb62d8a
Nathan-Simonnet <nathan.simonnet@gmail.com>
2024-11-07 10:33:04 +0100

    Game track which case has clicked each player, and restart if all squarres are clicked

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-
-const currentGame = ["", "", "", "", "", "", "", "", ""];
+// datas into object
 
 const winningCombinations = [
     [0, 1, 2],
@@ -13,30 +11,90 @@ const winningCombinations = [
     [2, 4, 6]
 ]
 
-
+let currentGameIndex = 0;
 let currentPlayer = "X";
+let playerXGame = [];
+let playerOGame = [];
+
+function pushScoreIntoGameArray(squarreNumber) {
+    currentPlayer === "X" ? playerXGame.push(squarreNumber) : playerOGame.push(squarreNumber);
+}
 
 function switchPlayer() {
     currentPlayer === "X" ? currentPlayer = "O" : currentPlayer = "X"
 }
 
+const messageContainer = document.querySelector('.message-container')
+const messageDisplay = document.querySelector('.message-display')
+function messageDisplayer(message) {
+    messageDisplay.textContent = message;
+}
+
 const grid = document.querySelector('.grid');
-const square = document.querySelector('.square');
-const marker = document.querySelector('.marker');
-
-console.log(grid, square, marker)
-
 
 function gridHandler(e) {
-    if(e.target.classList.contains("marker")){
+
+    if (e.target.classList.contains("marker")) {
         const squarreElement = e.target;
-        const squarreNumber = squarreElement.dataset.number;
-        squarreElement.classList.add('clicked')
-        console.log(squarreElement, squarreNumber);
         squarreElement.textContent = currentPlayer;
-        switchPlayer()
+        squarreElement.classList.add('clicked')
+        const squarreNumber = squarreElement.dataset.number;
+        // console.log(squarreElement, squarreNumber);
+        console.log(currentPlayer)
+
+        pushScoreIntoGameArray(squarreNumber);
+        switchPlayer();
+        messageDisplayer(`${currentPlayer}'s turn`)
+        currentGameIndex++;
+
+        if (currentGameIndex == 9) {
+            messageDisplayer("Press space to restart");
+            console.log(playerXGame, playerOGame)
+        }
     }
 }
 
 grid.addEventListener('click', gridHandler);
+window.addEventListener('keydown', (e) => {
+    if (e.key === ' ') {
+        document.querySelectorAll('.marker').forEach((mark) => {
+            mark.textContent = "";
+            mark.classList.remove('clicked');
+            messageDisplayer(`${currentPlayer}'s turn`);
+            currentGameIndex = 0;
+            playerXGame = [];
+            playerOGame = [];
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
